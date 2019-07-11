89f3b0110daeb28c8b321c531960600951a7f801
changfutao <changfutao1989@126.com>
2019-07-12 06:46:37 +0800

    Cookie身份认证

--- NetCoreProject/Controllers/AccountController.cs
+++ NetCoreProject/Controllers/AccountController.cs
@@ -4,18 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace NetCoreProject.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Login()
+        public async Task<IActionResult> Login()
         {
+            //基本信息
             var claims = new List<Claim>
             {
-               // new Claim("")
+               new Claim(ClaimTypes.Name,"changfutao"),
+               new Claim(ClaimTypes.Role,"admin")
             };
-            return View();
+            //身份证
+            var claimsIdentity=new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+            //写入Cookie
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,new ClaimsPrincipal(claimsIdentity));
+
+            return Ok();
+        }
+
+        public async Task<IActionResult> LoginOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
         }
     }
 }
